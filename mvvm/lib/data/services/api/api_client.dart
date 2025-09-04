9ce8fa55c245421e290c3a94c737d311e35d622d
Matheus Siqueira <siqueira4503@gmail.com>
2025-09-03 22:49:46 -0300

    freezed

--- mvvm/lib/data/services/api/api_client.dart
+++ mvvm/lib/data/services/api/api_client.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:mvvm/data/services/api/models/todo/todo_api_model.dart';
 import 'package:mvvm/domain/models/todo.dart';
 import 'package:mvvm/utils/result/result.dart';
 
@@ -41,15 +42,13 @@ class ApiClient {
     }
   }
 
-  Future<Result<Todo>> postTodo(Todo todo) async {
+  Future<Result<Todo>> postTodo(CreateTodoApiModel todo) async {
     final client = _clientFactory();
 
     try {
       final request = await client.post(_host, _port, "/todos");
 
-      request.write(jsonEncode({
-        'name': todo.name
-      }));
+      request.write(jsonEncode(todo.toJson()));
 
       final response = await request.close();
 
@@ -61,6 +60,33 @@ class ApiClient {
         final createTodo = Todo.fromJson(json);
 
         return Result.ok(createTodo);
+      }
+        
+      return Result.error(const HttpException("Valor invalidado"));
+    } on Exception catch (error) {
+      return Result.error(error);
+    } finally {
+      client.close();
+    }
+  }
+  Future<Result<Todo>> updateTodo(UpdateTodoApiModel todo) async {
+    final client = _clientFactory();
+
+    try {
+      final request = await client.put(_host, _port, "/todos/${todo.id}");
+
+      request.write(jsonEncode(todo.toJson()));
+
+      final response = await request.close();
+
+      if(response.statusCode == 200) {
+        final stringData = await response.transform(utf8.decoder).join();
+
+        final json = jsonDecode(stringData) as Map<String, dynamic>;
+
+        final updateTodo = Todo.fromJson(json);
+
+        return Result.ok(updateTodo);
       }
         
       return Result.error(const HttpException("Valor invalidado"));
--- mvvm/test/data/services/api_client_test.dart
+++ mvvm/test/data/services/api_client_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mvvm/data/services/api/api_client.dart';
+import 'package:mvvm/data/services/api/models/todo/todo_api_model.dart';
 import 'package:mvvm/domain/models/todo.dart';
 import 'package:mvvm/utils/result/result.dart';
 
@@ -18,20 +19,32 @@ void main() {
     });
 
     test("Should return Result ok when postTodo()", () async {
-      final Todo createTodo = Todo(name: "Funcionou mesmo");
+      final CreateTodoApiModel createTodo = CreateTodoApiModel(name: "Funcionou mesmo");
       final Result result = await apiClient.postTodo(createTodo);
 
       expect(result.asOk.value, isA<Todo>());
     });
 
     test("Should return Result ok when deleteTodo()", () async {
-      final Todo createTodo = Todo(name: "Vasco");
+      final CreateTodoApiModel createTodo = CreateTodoApiModel(name: "Vasco");
 
       final Result resultTodo = await apiClient.postTodo(createTodo);
 
       final Result result = await apiClient.deleteTodo(resultTodo.asOk.value);
 
       expect(result.asOk.value, isA<void>());
+    });
+    test("Should return Result ok when updateTodo()", () async {
+      final CreateTodoApiModel createTodo = CreateTodoApiModel(name: "Vasco nova era");
+
+      final Result resultTodo = await apiClient.postTodo(createTodo);
+
+      final Result result = await apiClient.updateTodo(UpdateTodoApiModel(
+        id: resultTodo.asOk.value.id, 
+        name: "Vasco 2"
+      ));
+
+      expect(result.asOk.value, isA<Todo>());
     });
   });
 }
